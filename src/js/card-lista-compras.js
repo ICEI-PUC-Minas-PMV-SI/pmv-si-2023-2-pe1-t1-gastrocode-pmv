c6ed4e56c4b44e00a60f92bb0bf92f0b513f7a9b
Viviane Pertence <vivianeaco@yahoo.com.br>
2023-11-05 19:32:20 -0300

    Atualização Lista de Compras

--- src/js/card-lista-compras.js
+++ src/js/card-lista-compras.js
@@ -1,34 +1,21 @@
-import receitasFavoritas from './receitas-favoritas-data.js';
+import listaCompras from './receitas-favoritas-data.js';
 
 const body = document.querySelector('body');
 const receitaContainer = document.getElementById('receitaContainer');
 
-function getReceitaCusto(custo) {
-    switch (custo) {
-        case 1:
-            return '$ (Baixo)';
-        case 2:
-            return '$$ (Médio)';
-        case 3:
-            return '$$$ (Alto)';
-        default:
-            return 'Não informado';
-    }
-}
-
-receitasFavoritas.forEach(receita => {
+listaCompras.forEach(receita => {
 
     const div = document.createElement('div');
-    div.classList.add('receita-favorita-container');
+    div.classList.add('lista-compras-container');
 
     div.innerHTML = `
         <img class="receita-item-imagem" src=${receita.imagem}>
 
-        <div class="receita-info-container">
-            <p class="favorita-info-item favorita-receita-title"> <strong>Nome da Receita: </strong>${receita.nome}</p>
-            <p class="favorita-info-item"> <strong>Ingredientes: </strong>${receita.ingredientes.forEach((receita) => `${receita}`)}</p>
-            <p class="favorita-info-item"> <strong>Modo de Preparo: </strong>${receita.modoPreparo}</p>
-            <p class="favorita-info-item"> <strong>Custo: </strong>${getReceitaCusto(receita.custo)}</p>
+        <div class="lista-info-container">
+            <div class="lista-info">
+                <p class="favorita-info-item favorita-receita-title"> <strong>Nome da Receita: </strong>${receita.nome}</p>
+                <p class="favorita-info-item"> <strong>Ingredientes: </strong>${receita.ingredientes}</p>
+             </div>          
             
             <div class="favorita-button-container">
                 
