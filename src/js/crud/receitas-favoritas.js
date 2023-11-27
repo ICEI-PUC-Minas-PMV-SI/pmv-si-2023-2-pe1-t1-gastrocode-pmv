8d4118b3538f276aacbfc6eb563c4a1a2f086598
Viviane Pertence <vivianeaco@yahoo.com.br>
2023-11-27 18:56:49 -0300

    Atualização página de receitas

---
+++ src/js/crud/receitas-favoritas.js
@@ -0,0 +1,23 @@
+const db = require('./db.json');
+
+function adicionarReceitaFavorita(usuarioID, receitaID) {
+    fetch('http://localhost:3000/favoritas')
+        .then(response => response.json())
+        .then(receitas => {
+  const usuario = db.usuarios.find(user => user.id === usuarioID);
+  if (usuarioid) {
+    if (!usuario.receitasFavoritas.includes(receitaID)) {
+      usuario.receitasFavoritas.push(receitaID);console.log('Receita adicionada aos favoritos!');
+    } else {
+      console.log('A receita já está nos favoritos do usuário.');
+    }
+  } else {
+    console.log('Usuário não encontrado.');
+  }
+})
+.catch(error => {
+  console.error('Erro:', error);
+});
+}
+
+adicionarReceitaFavorita(1, 2);
